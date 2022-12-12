d3bbfca383b2f96ea5ba05b8a437af03695a7c13
Tony Nguyen <tonixsmm>
2022-12-12 10:41:40 -0800

    error at semester grouping

--- utils.py
+++ utils.py
@@ -14,10 +14,6 @@ def load_apple():
     df["startDate"] = pd.to_datetime(df["startDate"])
     df["endDate"] = pd.to_datetime(df["endDate"])
     df["creationDate"] = pd.to_datetime(df["creationDate"])
-
-    # df["startDate"] = df["startDate"].dt.strftime("%Y-%m-%d")
-    # df["endDate"] = df["endDate"].dt.strftime("%Y-%m-%d")
-    # df["creationDate"] = df["creationDate"].dt.strftime("%Y-%m-%d")
     return df
 
 def sleep_filtering(df):
@@ -34,24 +30,59 @@ def sleep_cleaning(df):
     df.drop("device", axis=1, inplace=True)
     df.drop("MetadataEntry", axis=1, inplace=True)
     df.drop("HeartRateVariabilityMetadataList", axis=1, inplace=True)
+    df.drop("originalIndex", axis=1, inplace=True)
 
     # Reset indexes
-    # df.reset_index(inplace=True)
+    df.reset_index(inplace=True)
     return df
 
-def netflix_api():
-    url = "https://unogs-unogs-v1.p.rapidapi.com/search/titles"
+def time_calculation(df):
+    # Substraction
+    sleepTime = []
+    for item in range(len(df)):
+        sleepTime.append(df.iloc[item, 3] - df.iloc[item, 2])
+    sleepTime = pd.Series(sleepTime)
 
-    querystring = {"order_by":"date","title":"Wednesday","type":"movie"}
+    # Concatenate to the DataFrame
+    df = pd.concat([df, sleepTime], axis=1, ignore_index=False)
+    df.rename({0: "sleepTime"}, axis=1, inplace=True)
+    return sleepTime, df
 
-    headers = {
-        "X-RapidAPI-Key": "ab7bd550d6msh5810450efa9635ap180e67jsnbf2ac0823c39",
-        "X-RapidAPI-Host": "unogs-unogs-v1.p.rapidapi.com"
-    }
+def sem_split(df):
+    # Date format conversion
+    df["startDate"] = df["startDate"].dt.strftime("%Y-%m-%d")
+    df["endDate"] = df["endDate"].dt.strftime("%Y-%m-%d")
+    df["creationDate"] = df["creationDate"].dt.strftime("%Y-%m-%d")
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    # Semester split
+    fall_22 = df[(df.creationDate >= "2022-08-30") & (df.creationDate <= "2022-11-29")]
+    spring_22 = df[(df.creationDate >= "2022-01-12") & (df.creationDate <= "2022-05-05")]
 
-    print(response.text)
+    # Groupby
+    fall_22 = fall_22.groupby("creationDate").sum()
+    # fa22_grouped_by_creationDate = fall_22.groupby("creationDate").sum()
+    # sp22_grouped_by_creationDate = spring_22.groupby("creationDate").sum()
+    # fall_22 = pd.concat([fall_22, fa22_grouped_by_creationDate], axis=1, ignore_index=False)
+    # # df.rename({0: "sleepTime"}, axis=1, inplace=True)
+    # spring_22 = pd.concat([spring_22, sp22_grouped_by_creationDate], axis=1, ignore_index=False)
+
+    return fall_22, spring_22
+
+def dow_merge(fall_22, spring_22):
+    # Fall 22
+    date_fa22 = pd.read_csv("fall_22_dow.csv", index_col=0)
+    fall_22 = fall_22.merge(date_fa22, on=fall_22.index)
+    fall_22.set_index("key_0", inplace=True)
+    fall_22.index = pd.to_datetime(fall_22.index)
+    fall_22.rename({"key_0": "creationDate"}, axis=1, inplace=True)
+
+    # Spring 22
+    date_sp22 = pd.read_csv("spring_22_dow.csv", index_col=0)
+    spring_22 = spring_22.merge(date_sp22, on=spring_22.index)
+    spring_22.set_index("key_0", inplace=True)
+    spring_22.index = pd.to_datetime(spring_22.index)
+    spring_22.rename({"key_0": "creationDate"}, axis=1, inplace=True)
+    return fall_22, spring_22
 
 def data_visualization(df, sem):
     plt.figure
