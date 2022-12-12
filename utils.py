d8caeff640f762e45a7d5bc0fa8cf77467903695
Tony Nguyen <tonixsmm>
2022-12-11 22:32:20 -0800

    done Apple Health dropping

--- utils.py
+++ utils.py
@@ -1,10 +1,42 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import requests
+from datetime import timedelta
+from scipy import stats
 
-def load_xml(filename):
-    df = pd.read_xml(filename, xpath="Record")
+def load_apple():
+    # Load file
+    df = pd.read_csv("export_converted.csv")
+
+    df.rename(columns={"Unnamed: 0":"originalIndex"}, inplace=True)
+
+    # Convert format
+    df["startDate"] = pd.to_datetime(df["startDate"])
+    df["endDate"] = pd.to_datetime(df["endDate"])
+    df["creationDate"] = pd.to_datetime(df["creationDate"])
+
+    # df["startDate"] = df["startDate"].dt.strftime("%Y-%m-%d")
+    # df["endDate"] = df["endDate"].dt.strftime("%Y-%m-%d")
+    # df["creationDate"] = df["creationDate"].dt.strftime("%Y-%m-%d")
+    return df
+
+def sleep_filtering(df):
+    df = df[df.type == "HKCategoryTypeIdentifierSleepAnalysis"]
+    return df
+
+def sleep_cleaning(df):
+    # Drop columns
+    df.drop("type", axis=1, inplace=True)
+    df.drop("sourceName", axis=1, inplace=True)
+    df.drop("sourceVersion", axis=1, inplace=True)
+    df.drop("unit", axis=1, inplace=True)
+    df.drop("value", axis=1, inplace=True)
+    df.drop("device", axis=1, inplace=True)
+    df.drop("MetadataEntry", axis=1, inplace=True)
+    df.drop("HeartRateVariabilityMetadataList", axis=1, inplace=True)
+
+    # Reset indexes
+    # df.reset_index(inplace=True)
     return df
 
 def netflix_api():
